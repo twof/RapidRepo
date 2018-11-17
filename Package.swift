fa7f76f4460df4fdf0d828536afdef3ffc1c355e
Alex Reilly <fabiobean2@gmail.com>
2018-11-17 01:49:51 -0800

    RapidRepo product

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "RapidRepo",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "RapidRepo",
+            targets: ["Repository"]),
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
